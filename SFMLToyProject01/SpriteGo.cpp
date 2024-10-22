d817d6a9545299dde583d31d2054c71ba3485f14
문세진 <gamemsgpic@gmail.com>
2024-10-22 20:26:36 +0900

    SpriteGo 작성

--- SFMLToyProject01/SpriteGo.cpp
+++ SFMLToyProject01/SpriteGo.cpp
@@ -1,2 +1,38 @@
 #include "stdafx.h"
 #include "SpriteGo.h"
+
+SpriteGo::SpriteGo(const std::string& texId, const std::string& name)
+	: GameObject(name), textureId(texId)
+{
+}
+
+void SpriteGo::SetOrigin(Origins preset)
+{
+	originPreset = preset;
+	origin = Utils::SetOrigin(sprite, originPreset);
+}
+
+void SpriteGo::SetOrigin(const sf::Vector2f& newOrigin)
+{
+	originPreset = Origins::Custom;
+	origin = newOrigin;
+	sprite.setOrigin(origin);
+}
+
+void SpriteGo::Reset()
+{
+	sprite.setTexture(ResourceMgr<sf::Texture>::Instance().Get(textureId));
+	SetOrigin(originPreset);
+}
+
+void SpriteGo::SetPosition(const sf::Vector2f& pos)
+{
+	GameObject::SetPosition(pos);
+	sprite.setPosition(pos);
+}
+
+void SpriteGo::Draw(sf::RenderWindow& window)
+{
+	GameObject::Draw(window);
+	window.draw(sprite);
+}
--- SFMLToyProject01/SpriteGo.h
+++ SFMLToyProject01/SpriteGo.h
@@ -1,5 +1,22 @@
 #pragma once
-class SpriteGo
+class SpriteGo : public GameObject
 {
+protected:
+	sf::Sprite sprite;
+	std::string textureId;
+
+public:
+	SpriteGo(const std::string& texId, const std::string& name = "");
+	~SpriteGo() = default;
+
+	void SetOrigin(Origins preset);
+	void SetOrigin(const sf::Vector2f& newOrigin);
+	
+	
+	void Reset() override;
+	void SetPosition(const sf::Vector2f& pos) override;
+	void Draw(sf::RenderWindow& window) override;
+
+
 };
 
