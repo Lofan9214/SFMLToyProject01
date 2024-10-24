7812ee0b82a9f037f25d403fe44d1d142b0e6408
문세진 <gamemsgpic@gmail.com>
2024-10-24 11:21:25 +0900

    스타트화면 구현

--- SFMLToyProject01/SceneDev1.cpp
+++ SFMLToyProject01/SceneDev1.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "SceneDev1.h"
 #include "SpriteGo.h"
+#include "TextGo.h"
 
 
 SceneDev1::SceneDev1()
@@ -16,10 +17,14 @@ void SceneDev1::init()
 	obj->setOrigin(Origins::MC);
 	obj->setPosition({ 1920 / 2, 1080 / 2 });
 
-	obj = AddGo(new SpriteGo("graphics/Title.png"));
+	obj = AddGo(new SpriteGo("graphics/Title.png", "title"));
 	obj->setOrigin(Origins::MC);
 	obj->setPosition({ 1920 / 2, 1080 / 3 -100.f });
 	Scene::init();
+
+	obj = AddGo(new TextGo("fonts/KOMIKAP_.ttf", "startclick"));
+	obj->setOrigin(Origins::MC);
+	obj->setPosition({ 1920 / 2, 1080 / 2 + 350.f });
 }
 
 void SceneDev1::enter()
@@ -28,6 +33,7 @@ void SceneDev1::enter()
 
 	ResourceMgr<sf::Texture>::Instance().load("graphics/background2.png");
 	ResourceMgr<sf::Texture>::Instance().load("graphics/Title.png");
+	ResourceMgr<sf::Font>::Instance().load("fonts/KOMIKAP_.ttf");
 
 	Scene::enter();
 }
@@ -40,6 +46,7 @@ void SceneDev1::exit()
 
 	ResourceMgr<sf::Texture>::Instance().unload("graphics/background2.png");
 	ResourceMgr<sf::Texture>::Instance().unload("graphics/Title.png");
+	ResourceMgr<sf::Font>::Instance().unload("fonts/KOMIKAP_.ttf");
 
 }
 
@@ -52,19 +59,30 @@ void SceneDev1::update(float dt)
 		SceneMgr::Instance().changeScene(SceneIds::Dev2);
 	}
 
-	//acttime += dt;
-	//if (acttime > 5.f)
-	//{
-	//	acttime = 0.f;
-	//	for (std::list<GameObject*>::iterator it = gameObjects.begin(); it != gameObjects.end(); ++it)
-	//	{
-	//		auto ptr2 = dynamic_cast<SpriteGo*> (*it);
-	//		if (ptr2 != nullptr && ptr2->getName() == "graphics/title.png")
-	//		{
-	//
-	//		}
-	//	}
-	//}
+	acttime += dt;
+	acttime = 0.f;
+	float min;
+	float diff;
+	
+	for (std::list<GameObject*>::iterator it = gameObjects.begin(); it != gameObjects.end(); ++it)
+	{
+		auto ptr2 = dynamic_cast<SpriteGo*> (*it);
+		if (ptr2 != nullptr && ptr2->getName() == "title")
+		{
+			min = 1.2f;
+			diff = 0.3f;
+
+			ptr2->setScale({ min + diff * sinf(5.0f*Framework::Instance().getRealTime()), min + diff * sinf(5.0f * Framework::Instance().getRealTime()) });
+		}
+	}
+	for (std::list<GameObject*>::iterator it = gameObjects.begin(); it != gameObjects.end(); ++it)
+	{
+		auto txtptr = dynamic_cast<TextGo*>(*it);
+		if (txtptr != nullptr && txtptr->getName() == "startclick")
+		{
+			txtptr->setString("Press Click");
+		}
+	}
 	
 }
 
