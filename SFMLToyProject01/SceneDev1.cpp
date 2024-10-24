69d19d0c47c0f36b27d9681dc8464a2c242b7954
문세진 <gamemsgpic@gmail.com>
2024-10-24 10:32:27 +0900

    크지조절 작성중

--- SFMLToyProject01/SceneDev1.cpp
+++ SFMLToyProject01/SceneDev1.cpp
@@ -15,6 +15,10 @@ void SceneDev1::init()
 	GameObject* obj = AddGo(new SpriteGo("graphics/background2.png"));
 	obj->setOrigin(Origins::MC);
 	obj->setPosition({ 1920 / 2, 1080 / 2 });
+
+	obj = AddGo(new SpriteGo("graphics/Title.png"));
+	obj->setOrigin(Origins::MC);
+	obj->setPosition({ 1920 / 2, 1080 / 3 -100.f });
 	Scene::init();
 }
 
@@ -23,6 +27,7 @@ void SceneDev1::enter()
 	std::cout << "SceneDev1::Enter()" << std::endl;
 
 	ResourceMgr<sf::Texture>::Instance().load("graphics/background2.png");
+	ResourceMgr<sf::Texture>::Instance().load("graphics/Title.png");
 
 	Scene::enter();
 }
@@ -34,6 +39,7 @@ void SceneDev1::exit()
 	Scene::exit();
 
 	ResourceMgr<sf::Texture>::Instance().unload("graphics/background2.png");
+	ResourceMgr<sf::Texture>::Instance().unload("graphics/Title.png");
 
 }
 
